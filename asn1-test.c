47b206f86e1a26ce0baad2e017e6aa14f0f76858
Alexei A. Smekalkine <ikle@ikle.ru>
2017-07-18 00:10:37 +0300

    asn1-test: remove unused code

--- asn1-test.c
+++ asn1-test.c
@@ -22,47 +22,6 @@ static struct se *get_ast (FILE *file)
 	return se;
 }
 
-static void indent (int level)
-{
-	printf ("%*s", level * 4, "");
-}
-
-static void show (int level, const struct se *o);
-
-static void show_args (int level, const struct se *o)
-{
-	int i;
-
-	for (i = 0; i < se_count (o->type); ++i)
-		if (se_is_terminal (o->type))
-			printf (" %s", o->item[i]);
-		else if (se_count (o->type) == 1) {
-			putchar (' ');
-			show (0, o->item[i]);
-		}
-		else {
-			putchar ('\n');
-			show (level + 1, o->item[i]);
-		}
-}
-
-static void show (int level, const struct se *o)
-{
-	int i;
-
-	if (o == NULL || o->type == 0) {
-		se_show (level, o);
-		return;
-	}
-
-	indent (level); putchar ('(');
-
-	printf ("%05x", o->type);
-	show_args (level, o);
-
-	putchar (')');
-}
-
 int main (int argc, char *argv[])
 {
 	struct se *se;
@@ -70,7 +29,7 @@ int main (int argc, char *argv[])
 	if ((se = get_ast (stdin)) == NULL)
 		return 1;
 
-	show (0, se); putchar ('\n');
+	se_show (0, se); putchar ('\n');
 
 	se_free (se);
 	return 0;
